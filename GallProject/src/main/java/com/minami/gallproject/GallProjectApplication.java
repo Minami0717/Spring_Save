21ed2921f961dddcb14fc2d7b0876d5b3c17d849
남규진 <dft2007@naver.com>
2023-09-19 17:59:51 +0900

    gall project 작업중

---
+++ GallProject/src/main/java/com/minami/gallproject/GallProjectApplication.java
@@ -0,0 +1,13 @@
+package com.minami.gallproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GallProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GallProjectApplication.class, args);
+    }
+
+}
---
+++ GallProject/src/test/java/com/minami/gallproject/GallProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minami.gallproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GallProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
